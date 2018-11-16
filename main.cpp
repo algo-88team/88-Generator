e354e7456ec3ab808a9cc40761dd519c965f7448
PKH\arlec <gh9411@gmail.com>
2018-11-17 00:09:39 +0900

    맵 FRAME = input.txt

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 #include "create_puzzle.h"
 #include "create_blank.h"
 
+#define MAXSIZE 1000;
 
 using namespace std;
 
 int main()
 {
+	char map[MAXSIZE][MAXSIZE] = { 0, };
+	ifstream in("input.txt");
 	
+	in.close();
+
 
 	return 0;
 }
