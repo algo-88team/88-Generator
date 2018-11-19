b59bb78a4598f19da4ca5580783ac0e8dc2b9e32
PKH\arlec <gh9411@gmail.com>
2018-11-19 19:02:06 +0900

    create_puzzle 함수 3분할  / init 구현 input.txt 8*8 사이즈 임의 frame(예시 피피티 모양) 추가

--- create_puzzle.cpp
+++ create_puzzle.cpp
@@ -1,2 +1,45 @@
 #include <iostream> 
 #include "create_puzzle.h"
+
+void Create_puzzle::init_frame(int(*map)[MAXSIZE], int length, int width, int count) {
+	
+	//위아래,양옆 0으로 덮어 연산을 편하게 뚠뚠
+
+	//인공의 벽을 만들어줘야 하나? 해줘야되나 모르겠는데 우선 하다가 필요없으면 빼겠습니다
+
+	cout << length << width << endl;
+	
+	//사이즈 추가
+	y = length + 2;
+	x = width + 2; 
+	
+	
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < width; j++) {
+			
+			frame[i + 1][j + 1] = map[i][j];
+		}
+	}
+
+	//출력 확인
+	for (int i = 0; i < y; i++) {
+		for (int j = 0; j < x; j++) {
+			cout << frame[i][j];
+		}cout << endl;
+	}
+
+	cout << count << endl;
+}
+
+void Create_puzzle::generate() {
+	
+	
+	/*1.랜덤 시작점 선택
+	  2.랜덤 시작점부터 연속된 숫자 채워넣기
+	  3.모든 길이 막히면 1번 다시 실행
+	  4.모든 번호가 채워지면 함수 종료 */
+}
+
+void Create_puzzle::pass_frame() {
+
+}
--- main.cpp
+++ main.cpp
@@ -3,17 +3,47 @@
 #include "create_puzzle.h"
 #include "create_blank.h"
 
-#define MAXSIZE 1000;
+#define MAXSIZE 300
 
 using namespace std;
 
 int main()
 {
-	char map[MAXSIZE][MAXSIZE] = { 0, };
+	int map[MAXSIZE][MAXSIZE] = { 0, };
+
+	int width,length;
+	int count = 0; // blank의 총 갯수
+
 	ifstream in("input.txt");
+
+	//맵 프레임 입력 
+	//높이,넓이 순
+	//0 = 벽, 1 = blank
+	in >> length >> width;
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < width; j++) {
+			in >> map[i][j];
+			if (map[i][j] == 1)
+				count++;
+		}
+	}
+
+	// 콘솔 출력 확인 
+	cout << length << width <<count <<  endl;
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < width; j++) {
+			cout << map[i][j];
+		}cout << endl;
+	}
 	
 	in.close();
 
+	Create_puzzle test;
+	test.init_frame(map, length, width, count);
+
+
 
 	return 0;
+
+
 }
