a26ee410744bf37bb9305af2cf4ade3acc0037c4
thd0222 <thdtjddb@naver.com>
2018-11-21 17:43:49 +0900

    clion project set

---
+++ cmake-build-debug/CMakeFiles/88-Generator.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/thdtj/OneDrive/GitHub/88-Generator/create_blank.cpp" "/cygdrive/c/Users/thdtj/OneDrive/GitHub/88-Generator/cmake-build-debug/CMakeFiles/88-Generator.dir/create_blank.cpp.o"
+  "/cygdrive/c/Users/thdtj/OneDrive/GitHub/88-Generator/create_puzzle.cpp" "/cygdrive/c/Users/thdtj/OneDrive/GitHub/88-Generator/cmake-build-debug/CMakeFiles/88-Generator.dir/create_puzzle.cpp.o"
+  "/cygdrive/c/Users/thdtj/OneDrive/GitHub/88-Generator/main.cpp" "/cygdrive/c/Users/thdtj/OneDrive/GitHub/88-Generator/cmake-build-debug/CMakeFiles/88-Generator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/88-Generator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/88-Generator.dir/main.cpp.o"
+  "CMakeFiles/88-Generator.dir/create_puzzle.cpp.o"
+  "CMakeFiles/88-Generator.dir/create_blank.cpp.o"
+  "88-Generator.pdb"
+  "88-Generator.exe"
+  "lib88-Generator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/88-Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
